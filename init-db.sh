7c21f261cf3007fd9b459dd4d4bc10848ce058c2
Osmani Rosado Falcon <osmanir@uclv.cu>
2020-08-03 14:42:58 -0400

    Fix an error in init-db.sh.

--- init-db.sh
+++ init-db.sh
@@ -5,4 +5,4 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE DATABASE "$DVD_RENTAL_DB";
 EOSQL
 
-pg_restore -U postgres -d dvdrental /root/dvdrental.tar
+pg_restore -U postgres -d dvdrental /home/dvdrental.tar
