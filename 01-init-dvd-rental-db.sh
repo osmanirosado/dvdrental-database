ff1da2bcdf03f7a0cdad3e5d7883429157bfe3d3
Osmani Rosado Falcon <osmanir@uclv.cu>
2020-08-12 16:13:12 -0400

    Comment actions in initialization script.

--- 01-init-dvd-rental-db.sh
+++ 01-init-dvd-rental-db.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
+echo "Create database with name: '$DVD_RENTAL_DB' ..."
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE "$DVD_RENTAL_DB";
 EOSQL
 
+echo "Restore DVD Rental database on '$DVD_RENTAL_DB' ..."
 pg_restore -U postgres -d "$DVD_RENTAL_DB" /home/dvdrental.tar
